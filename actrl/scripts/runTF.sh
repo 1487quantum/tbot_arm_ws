700361aaf7a2ba1cded1ee6c5c8adf6d1949e793
Photon <1487quantum@users.noreply.github.com>
2017-05-16 21:55:48 +0800

    Delete runTF.sh

--- actrl/scripts/runTF.sh
+++
@@ -1,5 +0,0 @@
-source ~/tensorflow/bin/activate
-export CUDA_HOME=/usr/local/cuda-8.0
-export LD_LIBRARY_PATH="/usr/local/cuda-8.0/lib64/":$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="/usr/local/cudnn-5.1/lib64":$LD_LIBRARY_PATH
-python mainControlTF.py
